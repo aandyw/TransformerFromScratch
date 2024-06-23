be7a165876cb78b4ca0229f0b1e2eb7e5f8aab38
Andy W <andy999coc@gmail.com>
2024-06-23 10:48:54 -0400

    reorganized

--- encoder.py
+++ encoder.py
@@ -3,7 +3,7 @@ import copy
 import torch
 from torch import nn
 
-from layers.attention import MultiHeadAttention
+from attention import MultiHeadAttention
 
 
 class Encoder(nn.Module):
@@ -11,7 +11,7 @@ class Encoder(nn.Module):
     def __init__(self, n_heads, mask):
         super().__init__()
         self_attention = MultiHeadAttention()
-        self.attention_layers = nn.ModuleList([copy.deepcopy(self_attention) for _ in range()])
+        self.attention_layers = nn.ModuleList([copy.deepcopy(self_attention) for _ in range(n_heads)])
 
     def forward(self):
         pass
