452399400ec0d8c4d5c54c9f4e26b30f96ff9654
Andy W <andy999coc@gmail.com>
2024-12-14 01:44:33 -0500

    completed embeddings code

--- model/embeddings.py
+++ model/embeddings.py
@@ -32,26 +32,28 @@ class InputEmbeddings(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, seq_len: int = 512, dropout: float = 0.1):
+    def __init__(self, d_model: int, max_seq_len: int = 512, dropout: float = 0.1):
         """Positional encoding / embeddings for input tokens
 
         Args:
             d_model (int): Hidden dimension of the model. The size of the vector
                 representations (embeddings / hidden states) used throughout the
                 Transformer model.
-            seq_len (int, optional): Length of input sequences. Defaults to 512.
+            max_seq_len (int, optional): Maximum sequence length. Defaults to 512.
             dropout (float, optional): Dropout rate. Defaults to 0.1.
         """
         super().__init__()
         self.d_model = d_model
-        self.seq_len = seq_len
+        self.max_seq_len = max_seq_len
         self.dropout = nn.Dropout(dropout)
 
-        # Create positional encodings of shape (seq_len, d_model)
-        pe = torch.zeros(seq_len, d_model)
+        # Create positional encodings of shape (max_seq_len, d_model)
+        pe = torch.zeros(max_seq_len, d_model)
 
         ### REFER TO 3.5 Positional Encoding in [paper](https://arxiv.org/pdf/1706.03762) ###
-        pos = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)  # (seq_len, 1)
+
+        # Create tensor of shape (max_seq_len, 1)
+        pos = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(
             torch.arange(0, d_model, 2).float()
             * (-torch.log(Tensor(10000.0)) / d_model)
@@ -60,7 +62,7 @@ class PositionalEncoding(nn.Module):
         pe[:, 1::2] = torch.cos(pos * div_term)  # Odd positions
 
         # Add batch dimension to positional encodings
-        pe = pe.unsqueeze(0)  # (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)  # (1, max_seq_len, d_model)
 
         # Tensor is saved in file when model is saved.
         self.register_buffer("pe", pe)
@@ -75,5 +77,6 @@ class PositionalEncoding(nn.Module):
         """
 
         # Add positional encodings to input embeddings
-        x = x + self.pe[...]
+        seq_len = x.size(1)
+        x = x + self.pe[:, :seq_len, :]
         return self.dropout(x)
